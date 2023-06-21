d8f0e5c77b862fb93e162cc4175fbc904c8f2aef
antudas22 <20antu20@gmail.com>
2023-06-21 11:22:00 +0600

    updated navbar

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -3,30 +3,30 @@ import { FiMenu } from "react-icons/fi";
 
 const Navbar = () => {
     const menu = <>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to=''>Products</Link></li>
-            <li><Link to=''>About</Link></li>
-            <li><Link to=''>Contacts</Link></li>
-            <li><Link to=''>Blog</Link></li>
+            <li className="text-gray-500 hover:text-black transition duration-300"><Link to='/'>Home</Link></li>
+            <li className="text-gray-500 hover:text-black transition duration-300"><Link to=''>Products</Link></li>
+            <li className="text-gray-500 hover:text-black transition duration-300"><Link to=''>About</Link></li>
+            <li className="text-gray-500 hover:text-black transition duration-300"><Link to=''>Contacts</Link></li>
+            <li className="text-gray-500 hover:text-black transition duration-300"><Link to=''>Blog</Link></li>
     </>
     return (
         <div>
-            <div className="navbar bg-base-100">
+            <div className="navbar bg-white pt-[68px] pb-[32px] px-5">
   <div className="navbar-start">
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <a className="text-2xl text-orange-500 font-bold">Pinak Lab</a>
   </div>
   <div className="navbar-end bg-base-100">
   <div className="dropdown dropdown-end">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <FiMenu className='text-2xl'/>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 gap-1 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-lg dropdown-content mt-3 p-2 gap-1 shadow bg-base-100 rounded-box w-52">
         {menu}
       </ul>
     </div>
   </div>
   <div className="navbar-end hidden lg:flex">
-    <ul className="menu menu-horizontal px-1 gap-1">
+    <ul className="flex font-bold px-1 gap-8">
       {menu}
     </ul>
   </div>
