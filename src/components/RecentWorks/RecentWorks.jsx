c764ce0a5c92dac02cbebc7c54a0b5ffb62b482e
shaikat17 <shaikatpal56@gmail.com>
2023-06-26 12:24:40 +0600

    Layout change for counter section

--- src/components/RecentWorks/RecentWorks.jsx
+++ src/components/RecentWorks/RecentWorks.jsx
@@ -3,108 +3,150 @@ import CountUp from "react-countup";
 import ScrollTrigger from "react-scroll-trigger";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import rw1 from '../../assets/images/rw1.jpg';
-import rw2 from '../../assets/images/rw2.jpg';
-import rw3 from '../../assets/images/rw3.jpg';
-import rw4 from '../../assets/images/rw4.jpg';
-import rw5 from '../../assets/images/rw5.jpg';
-
+import rw1 from "../../assets/images/rw1.jpg";
+import rw2 from "../../assets/images/rw2.jpg";
+import rw3 from "../../assets/images/rw3.jpg";
+import rw4 from "../../assets/images/rw4.jpg";
+import rw5 from "../../assets/images/rw5.jpg";
 
 const RecentWorks = () => {
-    const [counterOn, setCounterOn] = useState(false);
-    const responsive = {
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
-        },
-        mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
-        }
-      };
-    return (
-        <div className="mx-8 lg:mx-28">
-            <div className="mb-20">
-            <h4 style={{letterSpacing: '0.2rem'}} className='text-sm font-bold text-slate-300'>Lorem ipsum dolor sit.3</h4>
-                <h1 className="text-3xl lg:text-5xl font-bold mt-3">Lorem, ipsum.</h1>
-            </div>
+  const [counterOn, setCounterOn] = useState(false);
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+  return (
+    <div className="mx-8 lg:mx-28">
+      <div className="mb-20">
+        <h4
+          style={{ letterSpacing: "0.2rem" }}
+          className="text-sm font-bold text-slate-300"
+        >
+          Lorem ipsum dolor sit.3
+        </h4>
+        <h1 className="text-3xl lg:text-5xl font-bold mt-3">Lorem, ipsum.</h1>
+      </div>
 
-            {/* Recent Works Cards */}
+      {/* Recent Works Cards */}
 
-            <div>
-            
-            <Carousel responsive={responsive}>
-            <div className=" lg:w-[400px]">
-                <img style={{width: '400px', height: '400px'}} src={rw1} className="rounded-lg" alt="" />
-                <h3 className="text-xl font-bold text-center mt-6">Lorem, ipsum dolor.</h3>
-            </div>
-            <div className=" lg:w-[400px]">
-                <img style={{width: '400px', height: '400px'}} src={rw2} className="rounded-lg" alt="" />
-                <h3 className="text-xl font-bold text-center mt-6">Lorem, ipsum dolor.</h3>
-            </div>
-            <div className=" lg:w-[400px]">
-                <img style={{width: '400px', height: '400px'}} src={rw3} className="rounded-lg" alt="" />
-                <h3 className="text-xl font-bold text-center mt-6">Lorem, ipsum dolor.</h3>
-            </div>
-            <div className=" lg:w-[400px]">
-                <img style={{width: '400px', height: '400px'}} src={rw4} className="rounded-lg" alt="" />
-                <h3 className="text-xl font-bold text-center mt-6">Lorem, ipsum dolor.</h3>
-            </div>
-            <div className=" lg:w-[400px]">
-                <img style={{width: '400px', height: '400px'}} src={rw5} className="rounded-lg" alt="" />
-                <h3 className="text-xl font-bold text-center mt-6">Lorem, ipsum dolor.</h3>
-            </div>
-            </Carousel>;
-            </div>
+      <div>
+        <Carousel responsive={responsive}>
+          <div className=" lg:w-[400px]">
+            <img
+              style={{ width: "400px", height: "400px" }}
+              src={rw1}
+              className="rounded-lg"
+              alt=""
+            />
+            <h3 className="text-xl font-bold text-center mt-6">
+              Lorem, ipsum dolor.
+            </h3>
+          </div>
+          <div className=" lg:w-[400px]">
+            <img
+              style={{ width: "400px", height: "400px" }}
+              src={rw2}
+              className="rounded-lg"
+              alt=""
+            />
+            <h3 className="text-xl font-bold text-center mt-6">
+              Lorem, ipsum dolor.
+            </h3>
+          </div>
+          <div className=" lg:w-[400px]">
+            <img
+              style={{ width: "400px", height: "400px" }}
+              src={rw3}
+              className="rounded-lg"
+              alt=""
+            />
+            <h3 className="text-xl font-bold text-center mt-6">
+              Lorem, ipsum dolor.
+            </h3>
+          </div>
+          <div className=" lg:w-[400px]">
+            <img
+              style={{ width: "400px", height: "400px" }}
+              src={rw4}
+              className="rounded-lg"
+              alt=""
+            />
+            <h3 className="text-xl font-bold text-center mt-6">
+              Lorem, ipsum dolor.
+            </h3>
+          </div>
+          <div className=" lg:w-[400px]">
+            <img
+              style={{ width: "400px", height: "400px" }}
+              src={rw5}
+              className="rounded-lg"
+              alt=""
+            />
+            <h3 className="text-xl font-bold text-center mt-6">
+              Lorem, ipsum dolor.
+            </h3>
+          </div>
+        </Carousel>
+      </div>
 
-            {/* Scroll Counters */}
-            <ScrollTrigger onEnter={()=>setCounterOn(true)}>
-        <div className="flex justify-around my-28 text-center">
-        <div className='flex gap-1 md:gap-3 lg:gap-5 items-center'>
-                <div>
-                    {
-                        counterOn &&
-                        <h2 className='text-2xl md:text-3xl lg:text-5xl font-bold text-[#ff5443]'><CountUp start={0} end={99} duration={2} delay={0} />%</h2>
-                    }
-                    <p className='text-xs md:text-sm lg:text-lg mt-5 text-slate-500'>Lorem, ipsum dolor.</p>
-                </div>
-                
-        </div>
-        <div className='flex gap-1 md:gap-3 lg:gap-5 items-center'>
-                <div>
-                    {
-                        counterOn &&
-                        <h2 className='text-2xl md:text-3xl lg:text-5xl font-bold text-[#ff5443]'><CountUp start={0} end={342} duration={2} delay={0} />+</h2>
-                    }
-                    <p className='text-xs md:text-sm lg:text-lg mt-5 text-slate-500'>Lorem, ipsum dolor.</p>
-                </div>
-            </div>
-            <div className='flex gap-1 md:gap-3 lg:gap-5 items-center'>
-                <div>
-                    {
-                        counterOn &&
-                        <h2 className='text-2xl md:text-3xl lg:text-5xl font-bold text-[#ff5443]'><CountUp start={0} end={24} duration={2} delay={0} />+</h2>
-                    }
-                    <p className='text-xs md:text-sm lg:text-lg mt-5 text-slate-500'>Lorem, ipsum dolor.</p>
-                </div>
-            </div>
-            <div className='flex gap-1 md:gap-3 lg:gap-5 items-center'>
-                <div>
-                    {
-                        counterOn &&
-                        <h2 className='text-2xl md:text-3xl lg:text-5xl font-bold text-[#ff5443]'><CountUp start={0} end={22} duration={2} delay={0} />+</h2>
-                    }
-                    <p className='text-xs md:text-sm lg:text-lg mt-5 text-slate-500'>Lorem, ipsum dolor.</p>
-                </div>
-            </div>
+      {/* Scroll Counters */}
+      <ScrollTrigger onEnter={() => setCounterOn(true)}>
+        {/* <div className="flex justify-around my-28 text-center"> */}
+        <div className="grid grid-cols-2 md:grid-cols-4 place-items-center my-10">
+          <div className="flex flex-col mb-3 md:mb-0">
+              {counterOn && (
+                <h2 className="text-3xl md:text-5xl font-bold text-[#ff5443]">
+                  <CountUp start={0} end={99} duration={2} delay={0} />%
+                </h2>
+              )}
+              <p className="lg:text-lg mt-5 text-slate-500">
+                Lorem, ipsum dolor.
+              </p>
+          </div>
+          <div className="flex flex-col mb-3 md:mb-0">
+              {counterOn && (
+                <h2 className="text-3xl md:text-5xl font-bold text-[#ff5443]">
+                  <CountUp start={0} end={342} duration={2} delay={0} />+
+                </h2>
+              )}
+              <p className="lg:text-lg mt-5 text-slate-500">
+                Lorem, ipsum dolor.
+              </p>
+          </div>
+          <div className="flex flex-col">
+              {counterOn && (
+                <h2 className="text-3xl md:text-5xl font-bold text-[#ff5443]">
+                  <CountUp start={0} end={24} duration={2} delay={0} />+
+                </h2>
+              )}
+              <p className="lg:text-lg mt-5 text-slate-500">
+                Lorem, ipsum dolor.
+              </p>
+          </div>
+          <div className="flex flex-col">
+              {counterOn && (
+                <h2 className="text-3xl md:text-5xl font-bold text-[#ff5443]">
+                  <CountUp start={0} end={22} duration={2} delay={0} />+
+                </h2>
+              )}
+              <p className="lg:text-lg mt-5 text-slate-500">
+                Lorem, ipsum dolor.
+              </p>
+          </div>
         </div>
       </ScrollTrigger>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default RecentWorks;
